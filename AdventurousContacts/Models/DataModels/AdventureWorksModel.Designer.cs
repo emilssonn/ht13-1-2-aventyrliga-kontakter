d46e66e41fddd5dff1bf4f073e8177d0bb317c2c
emilssonn <pe222bz@student.lnu.se>
2013-11-24 15:32:41 +0100

    Added models and ADO.NET Entity Framework ORM

---
+++ AdventurousContacts/Models/DataModels/AdventureWorksModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Peter\Dropbox\Uni\1DV409\ht13-1-2-aventyrliga-kontakter\AdventurousContacts\Models\DataModels\AdventureWorksModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ AdventurousContacts/Models/Repository/Repository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AdventurousContacts.Models.DataModels;
+using System.Data;
+
+namespace AdventurousContacts.Models.Repository
+{
+    public class Repository : IRepository
+    {
+        private AdventureWorksEntities _entities = new AdventureWorksEntities();
+
+        #region IRepository members
+
+        public void Add(Contact contact)
+        {
+            this._entities.Contacts.Add(contact);
+        }
+
+        public void Delete(Contact contact)
+        {
+            var entity = this._entities.Contacts.Find(contact.ContactID);
+            this._entities.Contacts.Remove(entity);
+        }
+
+        public IQueryable<Contact> FindAllContacts()
+        {
+            //return this._entities.Contacts;
+            return this._entities.Contacts.ToList().AsQueryable();
+        }
+
+        public Contact GetContactById(int contactId)
+        {
+            return this._entities.Contacts.Find(contactId);
+        }
+
+        public List<Contact> GetLastContacts(int count = 20)
+        {
+            return this._entities.Contacts.OrderByDescending(c => c.ContactID).Take(count).OrderBy(c => c.ContactID).ToList();
+        }
+
+        public void Save()
+        {
+            this._entities.SaveChanges();
+        }
+
+        public void Update(Contact contact)
+        {
+            this._entities.Entry(contact).State = EntityState.Modified;
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        private bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                    this._entities.Dispose();
+            }
+            this._disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
